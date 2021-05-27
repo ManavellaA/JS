4dff324ff0b1ef2869bbd97d240971556b3083ef
ManavellaA <andresraulmanavella@gmail.com>
2021-05-26 21:07:23 -0300

    posible desafio 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-let nombre = "Andres";
-let apellido = "Manavella";
-let edad = "30";
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Gracias, ahora tu apellido");
+let edad = prompt("Excelente que edad tenes?");
 
-let final = "Soy " + nombre + " " + apellido + " y tengo " + edad;
+const resultConsole = "Usuario: " + nombre + " " + apellido;
+const resultConsole2 = "Edad: " + edad;
+const resultAlert = "Ya estas participando " + nombre + " " + "gracias por participar!!";
+
+console.log(resultConsole);
+console.log(resultConsole2);
+
+alert(resultAlert);
