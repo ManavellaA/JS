0f21c99ffd955999a76ec812f1e3a824e367a50c
ManavellaA <andresraulmanavella@gmail.com>
2021-05-27 21:08:35 -0300

    Desafio 1 JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 let nombre = prompt("Ingrese su Nombre");
 let apellido = prompt("Ingrese su Apellido");
-let numero = prompt("Ingrese su numero al que va a apostar");
+let jugada = prompt("Ingrese su numero al que va a apostar");
+let numero = parseInt(jugada);
 alert("Ingresaste" + " " + numero);
 
 const result = nombre + " " + apellido + " " +"apostó al" + " " + numero;
-const resultAlert = nombre + " " + apellido + " " +"tu apuesta al" + " " + numero + " " + "ya se encuentra activa";
+const resultAlert = nombre + " " + apellido + " " +"tu apuesta al" + " " + "numero" + " " + numero + " " + "ya se encuentra activa";
 
 console.log(result);
 alert(resultAlert);
