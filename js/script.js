e24cb03126001f7b2c624f49c8cbb4d5b202187f
ManavellaA <andresraulmanavella@gmail.com>
2021-05-27 21:52:17 -0300

    Desafio 1 JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 let nombre = prompt("Ingrese su Nombre");
 let apellido = prompt("Ingrese su Apellido");
-let jugada = prompt("Ingrese su numero al que va a apostar");
-let numero = parseInt(jugada);
-alert("Ingresaste" + " " + numero);
+let pasada1 = prompt("Ingresa tu primera pasada en formato m.ss");
+let pasada2 = prompt("Ingresa tu segunda pasada en formato m.ss");
+let pasada3 = prompt("Ingresa tu tercera pasada en formato m.ss");
 
-const result = nombre + " " + apellido + " " +"apostó al" + " " + numero;
-const resultAlert = nombre + " " + apellido + " " +"tu apuesta al" + " " + "numero" + " " + numero + " " + "ya se encuentra activa";
+const dato1 = nombre + " " + apellido;
+const tiempo1 = parseFloat(pasada1);
+const tiempo2 = parseFloat(pasada2);
+const tiempo3 = parseFloat(pasada3);
 
-console.log(result);
-alert(resultAlert);
+let promedio = (tiempo1 + tiempo2 + tiempo3) / 3;
+
+console.log("Promedio de" , dato1 , promedio, "min/km");
+const msjAlert = "Tu promedio es" + " " + promedio + " " + "min/km";
+alert(msjAlert);
+
