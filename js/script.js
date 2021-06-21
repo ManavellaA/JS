c5786346fa66f560a5f146642e26ad8905ab7573
ManavellaA <andresraulmanavella@gmail.com>
2021-06-21 14:22:19 -0300

    Desafio Arrays y Primera entrega de proyecto

--- js/script.js
+++ js/script.js
@@ -2,37 +2,74 @@
 let user;
 let peso;
 let estatura;
+
 logUser();
 userData();
 
-alert(`ingresa tus pasadas, cuando termines, solo presiona la tecla "Enter" dejando el campo vacio`);
-
 let promedio;
-dataCharge();
+let msj;
+let imc;
 
-let msj; 
-calif();
+let eleccion = prompt(`Si queres sabes tu IMC escribe "IMC", si queres calificar tu Ciclo de Enternamiento escribe "Ciclo"`);
 
-const msjAlert = user + " tu promedio es " + promedio + " min/km y tu desempeño clasifica como " + msj;
-alert(msjAlert);
+if(eleccion === "Ciclo" || eleccion === "ciclo" || eleccion === "CICLO"){
+    ciclo();
+
+    eleccion = prompt(`Si todavia queres saber tu IMC escribe "IMC", sino solo presiona Enter`);
+    if(eleccion === "IMC" || eleccion === "imc" || eleccion === "Imc"){
+    Imc();
+    }
+    else{
+        imc = "N/A"
+    };
+}
+
+else{
+    Imc();
+    eleccion = prompt(`Si todavia queres ingresar tu ciclo de entenamiento escribe "Ciclo", sino solo presiona Enter`);
+        if(eleccion === "Ciclo" || eleccion === "ciclo" || eleccion === "CICLO"){
+            ciclo();
+        }
+        else{
+            promedio = "N/A";
+            msj = "N/A";
+        }
+}
+
+alert(`Gracias por visitar nuestro sitio`);
 //------------------------------------
 
 //--------------Objeto----------------
+const arrayUsuario = []
+
 class dataUser{
-    constructor(dato1, dato2, dato3, dato4, dato5){
+    constructor(dato1, dato2, dato3, dato4, dato5, dato6){
         this.usuario = dato1;
         this.peso = dato2;
         this.estatura = dato3;
         this.promedio = dato4;
-        this.desempeño = dato5
+        this.desempeño = dato5;
+        this.imc = dato6;
     }
     leer(){
-        console.log(`El usuario ${this.usuario} pesa ${this.peso}Kg, mide ${estatura}cm y su desempeño calsifica como ${this.desempeño}`)
+        console.log(`El usuario ${this.usuario} pesa ${this.peso}Kg, mide ${estatura}cm, su IMC es de ${imc} y su desempeño calsifica como ${this.desempeño}`)
     }
 }
 
-let user1 = new dataUser(user, peso, estatura, promedio, msj);
-user1.leer();
+arrayUsuario.push(new dataUser(user, peso, estatura, promedio, msj, imc))
+
+for (let usuario of arrayUsuario){
+    document.write("<br>");
+    document.write(`<div><p>Nombre de Usuario: ${usuario.usuario}</p>`);
+    document.write(`<p>Peso: ${usuario.peso}</p>`);
+    document.write(`<p>Estatura: ${usuario.estatura}</p>`);
+    document.write(`<p>Promedio: ${usuario.promedio}</p>`);
+    document.write(`<p>Desempeño deportivo: ${usuario.desempeño}</p>`);
+    document.write(`<p>IMC: ${usuario.imc}</p></div>`);
+    document.write("<br>");
+    usuario.leer();
+}
+
 //------------------------------------
 
 //-------------Funciones--------------
@@ -42,16 +79,18 @@ function logUser (){
         alert(`Dato no Ingresado`);
         nombre = prompt(`Ingresar un nombre de usuario`);
     }
-    user = nombre
+    user = nombre;
 }
 
 function userData(){
+
     let dato1 = prompt(`Ingresa tu peso actual (solo numeros)`);
     while( ! dato1 || dato1 === " "){
         alert(`Dato no Ingresado`);
         dato1 = prompt(`Ingresa tu peso actual (solo numeros)`);
     }
     parseInt(dato1);
+
     let dato2 = prompt(`Ingresa tu estatura en centimetros (solo numeros)`);
     while( ! dato2 || dato2 === " "){
         alert(`Dato no Ingresado`);
@@ -59,8 +98,8 @@ function userData(){
     }
     parseInt(dato2);
 
-    peso = dato1
-    estatura = dato2
+    peso = dato1;
+    estatura = dato2;
 }
 
 function dataCharge(){
@@ -97,4 +136,37 @@ function calif(){
     }
 
     msj = res
+}
+
+function ciclo(){
+    alert(`ingresa tus pasadas, cuando termines, solo presiona la tecla "Enter" dejando el campo vacio`);
+
+    dataCharge(); 
+    calif();
+
+    const msjAlert = user + " tu promedio es " + promedio + " min/km y tu desempeño clasifica como " + msj;
+    alert(msjAlert);
+}
+
+function Imc(){
+    imc = (peso * ((estatura/100) * (estatura/100)))/10;
+    
+    if(imc < 18.5){
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Bajo Peso"`)
+    }
+    else if(imc < 24.9){   
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Peso Normal"`)
+    }
+    else if(imc < 29.9){
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Sobrepeso"`)
+    }
+    else if(imc < 34.9){
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 1"`)
+    }
+    else if(imc < 39.9){
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 2"`)
+    }
+    else{
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 3"`)
+    }
 }
