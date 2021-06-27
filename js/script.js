ce3e16ce27396d62eaa753449a5af936ced4497e
ManavellaA <andresraulmanavella@gmail.com>
2021-06-27 15:18:42 -0300

    Proyecto corrigiendo el uso de variables en el Scope Global

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-//----Calculo de rutina de Running----
-let user;
-let peso;
-let estatura;
-
+function logUser (){
+    let nombre = prompt(`Ingresa un nombre de usuario`);
+    while( ! nombre || nombre === " "){
+        alert(`Dato no Ingresado`);
+        nombre = prompt(`Ingresar un nombre de usuario`);
+    }
+    return user = nombre;
+}
 logUser();
+
+function userData(){
+
+    let dato1 = prompt(`Ingresa tu peso actual (solo numeros)`);
+    while( ! dato1 || dato1 === " "){
+        alert(`Dato no Ingresado`);
+        dato1 = prompt(`Ingresa tu peso actual (solo numeros)`);
+    }
+    parseInt(dato1);
+
+    let dato2 = prompt(`Ingresa tu estatura en centimetros (solo numeros)`);
+    while( ! dato2 || dato2 === " "){
+        alert(`Dato no Ingresado`);
+        dato2 = prompt(`Ingresa tu estatura en centimetros (solo numeros)`);
+    }
+    parseInt(dato2);
+
+    return peso = dato1, estatura = dato2;
+}
 userData();
 
-let promedio;
-let msj;
-let imc;
 
+function esqueleto(){
 let eleccion = prompt(`Si queres sabes tu IMC escribe "IMC", si queres calificar tu Ciclo de Enternamiento escribe "Ciclo"`);
 
 if(eleccion === "Ciclo" || eleccion === "ciclo" || eleccion === "CICLO"){
@@ -37,9 +57,80 @@ else{
 }
 
 alert(`Gracias por visitar nuestro sitio`);
+}
+esqueleto();
 //------------------------------------
 
-//--------------Objeto----------------
+function dataCharge(){
+    let pasada = 0;
+    let i = 1;
+    let suma = 0;
+    while (pasada >= 0){
+        pasada = prompt(`Ingresa tu pasada n° ${i}`);
+        if (! pasada || pasada === " "){
+            break
+        }
+        pasada = parseFloat(pasada); 
+        suma = suma + pasada;
+        i++;
+    }
+    i--;
+    let res = suma / i;
+
+    return promedio = res; 
+}
+
+function calif(){
+    if (promedio < 4.5){
+        res = "Destacable";
+    }
+    else if (promedio <= 6){
+        res = "Muy bueno";
+        }
+    else if (promedio <= 7){
+        res ="Normal";
+            }    
+    else{
+        res = "Malo" ;
+    }
+
+    return msj = res;
+}
+
+function ciclo(){
+    alert(`ingresa tus pasadas, cuando termines, solo presiona la tecla "Enter" dejando el campo vacio`);
+
+    dataCharge(); 
+    calif();
+
+    msjAlert = user + " tu promedio es " + promedio + " min/km y tu desempeño clasifica como " + msj;
+    alert(msjAlert);
+}
+
+function Imc(){
+    imc = (peso * ((estatura/100) * (estatura/100)))/10;
+    
+    if(imc < 18.5){
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Bajo Peso"`)
+    }
+    else if(imc < 24.9){   
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Peso Normal"`)
+    }
+    else if(imc < 29.9){
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Sobrepeso"`)
+    }
+    else if(imc < 34.9){
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 1"`)
+    }
+    else if(imc < 39.9){
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 2"`)
+    }
+    else{
+        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 3"`)
+    }
+}
+
+//--------------Array----------------
 const arrayUsuario = []
 
 class dataUser{
@@ -71,102 +162,3 @@ for (let usuario of arrayUsuario){
 }
 
 //------------------------------------
-
-//-------------Funciones--------------
-function logUser (){
-    let nombre = prompt(`Ingresa un nombre de usuario`);
-    while( ! nombre || nombre === " "){
-        alert(`Dato no Ingresado`);
-        nombre = prompt(`Ingresar un nombre de usuario`);
-    }
-    user = nombre;
-}
-
-function userData(){
-
-    let dato1 = prompt(`Ingresa tu peso actual (solo numeros)`);
-    while( ! dato1 || dato1 === " "){
-        alert(`Dato no Ingresado`);
-        dato1 = prompt(`Ingresa tu peso actual (solo numeros)`);
-    }
-    parseInt(dato1);
-
-    let dato2 = prompt(`Ingresa tu estatura en centimetros (solo numeros)`);
-    while( ! dato2 || dato2 === " "){
-        alert(`Dato no Ingresado`);
-        dato2 = prompt(`Ingresa tu estatura en centimetros (solo numeros)`);
-    }
-    parseInt(dato2);
-
-    peso = dato1;
-    estatura = dato2;
-}
-
-function dataCharge(){
-    let pasada = 0;
-    let i = 1;
-    let suma = 0;
-    while (pasada >= 0){
-        pasada = prompt(`Ingresa tu pasada n° ${i}`);
-        if (! pasada || pasada === " "){
-            break
-        }
-        pasada = parseFloat(pasada); 
-        suma = suma + pasada;
-        i++;
-    }
-    i--;
-    let res = suma / i;
-
-    promedio = res; 
-}
-
-function calif(){
-    if (promedio < 4.5){
-        res = "Destacable";
-    }
-    else if (promedio <= 6){
-        res = "Muy bueno";
-        }
-    else if (promedio <= 7){
-        res ="Normal";
-            }    
-    else{
-        res = "Malo" ;
-    }
-
-    msj = res
-}
-
-function ciclo(){
-    alert(`ingresa tus pasadas, cuando termines, solo presiona la tecla "Enter" dejando el campo vacio`);
-
-    dataCharge(); 
-    calif();
-
-    const msjAlert = user + " tu promedio es " + promedio + " min/km y tu desempeño clasifica como " + msj;
-    alert(msjAlert);
-}
-
-function Imc(){
-    imc = (peso * ((estatura/100) * (estatura/100)))/10;
-    
-    if(imc < 18.5){
-        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Bajo Peso"`)
-    }
-    else if(imc < 24.9){   
-        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Peso Normal"`)
-    }
-    else if(imc < 29.9){
-        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Sobrepeso"`)
-    }
-    else if(imc < 34.9){
-        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 1"`)
-    }
-    else if(imc < 39.9){
-        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 2"`)
-    }
-    else{
-        alert(`Tu IMC es ${imc}Kg/m², Clasifica dentro de "Obesidad Tipo 3"`)
-    }
-}
